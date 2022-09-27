1440cebbe7a9408abb3156548ce4e38780a39888
Milton Ouma <ouma.nyang1@gmail.com>
2022-09-27 19:12:55 +0300

    commit tree 3b18346c6d96d582f91942b9e03115485a962fd1

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
 end
